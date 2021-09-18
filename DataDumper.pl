c3c26267a5e35f5eef59a1b9a3ca6ce4a068edb1
ravik0105 <90980276+ravik0105@users.noreply.github.com>
2021-09-18 23:08:34 +0530

    Create DataDumper.pl

---
+++ DataDumper.pl
@@ -0,0 +1,20 @@
+use warnings;
+use strict;
+use Data::Dumper; 
+
+my $text = <<END;
+name: Antonio Vivaldi, period: 1678-1741
+name: Ravi Kannan Zani, period: 1696-1757
+name: Antonio Brioschi, period: 1725-1750
+END
+
+my %composers;
+
+for my $line (split /\n/, $text){
+#    print $line, "\n";
+    if($line =~ /name:\s+([\w+\s]+),\s+period:\s*(\d{4}\-\d{4})/){
+    	$composers{$1} = $2;
+    }
+}
+
+print Dumper(\%composers);
